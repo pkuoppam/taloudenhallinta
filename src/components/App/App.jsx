428e7f5d28f16d26f6fffba8eb33cd7e948f352e
pkuoppam <petri.kuoppamaa@edu.sasky.fi>
2023-09-28 20:57:29 +0300

    lisää yleiset ulkoasumääritteet

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,14 @@
 import Item from '../Item'
-import './App.css'
+import styles from './App.module.scss'
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className={styles.app}>
+        <Item />
+        <Item />
+        <Item />
         <Item />
       </div>
     </>

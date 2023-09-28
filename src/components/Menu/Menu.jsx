16bb8272cdfa91c7b688fbd0f2f0538cc06c3244
pkuoppam <petri.kuoppamaa@edu.sasky.fi>
2023-09-28 22:18:36 +0300

    lisää linkityksen Menu-komponenttiin

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,14 +2,15 @@ import styles from './Menu.module.scss'
 import { MdSettings } from 'react-icons/md'
 import { MdTimeline } from 'react-icons/md'
 import { MdViewList } from 'react-icons/md'
+import { NavLink } from 'react-router-dom'
 
 function Menu() {
 
   return (
     <div className={styles.menu}>
-      <div><MdViewList /></div>
-      <div><MdTimeline /></div>
-      <div><MdSettings /></div>
+      <div><NavLink to=""><MdViewList /></NavLink></div>
+      <div><NavLink to="/stats"><MdTimeline /></NavLink></div>
+      <div><NavLink to="/settings"><MdSettings /></NavLink></div>
     </div>
   )
 

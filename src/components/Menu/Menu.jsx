8b67644a377bfc67fb3e7306841220839018a01c
pkuoppam <petri.kuoppamaa@edu.sasky.fi>
2023-09-28 21:12:07 +0300

    muuttaa menupalkin tekstit ikoneiksi

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,12 +1,15 @@
 import styles from './Menu.module.scss'
+import { MdSettings } from 'react-icons/md'
+import { MdTimeline } from 'react-icons/md'
+import { MdViewList } from 'react-icons/md'
 
 function Menu() {
 
   return (
     <div className={styles.menu}>
-      <div>list</div>
-      <div>stats</div>
-      <div>profile</div>
+      <div><MdViewList /></div>
+      <div><MdTimeline /></div>
+      <div><MdSettings /></div>
     </div>
   )
 

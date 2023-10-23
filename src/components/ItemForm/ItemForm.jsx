0b03de3729c7252dd0d2cc6f69141784c4d51880
pkuoppam <petri.kuoppamaa@edu.sasky.fi>
2023-10-23 16:36:01 +0300

    lisää lomakkeelle pakollisten kenttien tarkistuksen

--- src/components/ItemForm/ItemForm.jsx
+++ src/components/ItemForm/ItemForm.jsx
@@ -78,7 +78,14 @@ function ItemForm(props) {
               <Button onClick={handleCancel}>PERUUTA</Button>
             </div>
             <div>
-            <Button primary type='submit'>{ props.formData ? "TALLENNA" : "LISÄÄ" }</Button>
+            <Button primary
+                    disabled={values.type &&
+                              values.amount &&
+                              values.paymentDate &&
+                              values.receiver ? "" : "true"}
+                    type='submit'>
+              { props.formData ? "TALLENNA" : "LISÄÄ" }
+            </Button>
             </div>
           </div>
         </div>
